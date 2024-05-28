5204ae2de7e932459e54ef910d7feb15c3dbf8d0
DangBinh2002 <108292994+DangVanBinh@users.noreply.github.com>
2024-05-28 18:21:44 +0700

    Time: 78 ms, Memory: 43 MB - LeetHub

---
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool CheckIfExist(int[] arr) {
+        int n = arr.Length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i]==2*arr[j]){
+                    return true;
+                }else if(arr[j]==2*arr[i]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
